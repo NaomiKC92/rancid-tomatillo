2aef35afe2d33ddbdb140ce2322804341ed7b1b6
Nick Nist <nicknistoc@gmail.com>
2019-12-19 14:24:39 -0700

    Add rating api calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -24,3 +24,12 @@ export const getUser = (user) => {
       return res.json();
     })
 };
+
+export const getUserRatings = id => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v1/users/${id}/ratings`).then(res => {
+    if (!res.ok) {
+      throw Error('Could not find your ratings')
+    }
+    return res.json();
+  })
+}
